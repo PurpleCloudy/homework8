ae5f0b77e8cdcb6535a91f4061ba86923f7fa763
PurpleCloudy <88829502+PurpleCloudy@users.noreply.github.com>
2021-09-05 08:34:02 +0300

    Add files via upload

--- minigame2.0.py
+++ minigame2.0.py
@@ -1,47 +1,89 @@
+#рисовалка
+def painting():
+    for i in pole:
+        print(' '.join(i))
+
+#создание поля
 size = int(input())
 pole = []
 for i in range(size):
     pole = pole + [['_']*size]
+#х - номер списка сверху вниз
 x = 0
+#у - номер элемента в списке слева направо
 y = 0
+#
 progress = True
+
+#создание крестика в заданных координатах
 pole[x][y] = 'x'
-for i in pole:
-    print(' '.join(i))
+painting()
+
+#список того, что прога может
+command_list = ['up', 'down', 'left','right','exit']
+
+
+#подьем крестика
+def up(x,y):      
+    pole[x][y] = '_'
+    if x < 0:
+        x = size - 1
+    x -= 1
+    pole[x][y] = 'x'
+    painting()
+    print(x,y)
+    return x,y
+
+#спуск крестика
+def down(x,y):
+    pole[x][y] = '_'
+    if x > size - 1:
+        x = 0
+    x += 1
+    pole[x][y] = 'x'
+    painting()
+    print(x,y)
+    return x,y
+
+#крестик влево
+def left(x,y):
+    pole[x][y] = '_'
+    if y < 0:
+        y = size - 1
+    y -= 1
+    pole[x][y] = 'x'
+    painting()
+    print(x,y)
+    return x,y
+
+#крестик вправо
+def right(x,y):
+    pole[x][y] = '_'
+    if y > size - 1:
+        y = 0
+    y = y + 1
+    pole[x][y] = 'x'
+    painting()
+    print(x,y)
+    return x,y+1
+
+#выход
+def exit():
+    progress = False
+    return progress
+
+#сама собственно программа
 while progress == True:
     user_input = input()
-    if user_input == 'up': 
-        pole[x][y] = '_'
-        if x < 0:
-           x = size - 1
-        x -= 1
-        pole[x][y] = 'x'
-        for i in pole:
-            print(' '.join(i))
+    if user_input == 'up':
+        up(x,y)
     if user_input == 'down':
-        pole[x][y] = '_'
-        if x > size - 1:
-            x = 0
-        x += 1
-        pole[x][y] = 'x'
-        for i in pole:
-            print(' '.join(i))
-    if user_input == 'left': 
-        pole[x][y] = '_'
-        if y < 0:
-           y = size - 1
-        y -= 1
-        pole[x][y] = 'x'
-        for i in pole:
-            print(' '.join(i))
+        down(x,y)
+    if user_input == 'left':
+        left(x,y)
     if user_input == 'right':
-        pole[x][y] = '_'
-        if y > size - 1:
-            y = 0
-        y += 1
-        pole[x][y] = 'x'
-        for i in pole:
-            print(' '.join(i))
+        right(x,y)
+        print(x,y)
     if user_input == 'exit':
-        progress = False
+        exit()
 print('good bye')
---
+++ randomgame.py
@@ -0,0 +1,26 @@
+import sys
+import random 
+sys.stdout = open('D:/python/new_f', 'w')
+print('print start')
+user_input = input()
+    # with open('new_f', 'w+') as rand:
+    #     # write_data = txt
+    #     rand.write(txt)
+    #     print(txt)
+# print(write_data)
+
+def game():
+    random_number = random.randint(1,100)
+    print('I create a random number, try to guess it')
+    progress = True
+    while progress == True:
+        guess = int(input())
+        if guess > random_number:
+            print ('your guess > then my guess on', guess - random_number)
+        if guess < random_number:
+            print('your number < then my guess on', random_number - guess)
+        else: 
+            progress = False
+    return str()
+game()
+sys.stdout.close()
